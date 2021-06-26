5b65fa3c44cf0bc1d2091c564e24e0b699cd526f
williamkdt <42241841+williamkdt@users.noreply.github.com>
2021-06-26 17:45:06 -0400

    Update ALG_Sequencia_Guloso.c

--- ALG_Sequencia_Guloso.c
+++ ALG_Sequencia_Guloso.c
@@ -3,6 +3,7 @@
 #define N 5
 
 int VerDir (int grid[N][N], int i, int j){
+    //Verifica se a posição pode ir para a direita
     if(i >= N || j+1 >= N)
         return 0;
     else if(abs(grid[i][j] - grid[i][j+1]) == 1)
@@ -12,6 +13,7 @@ int VerDir (int grid[N][N], int i, int j){
 }
 
 int VerBx (int grid[N][N], int i, int j){
+    //Verifica se a posição pode ir para a esquerda
     if(i+1 >= N || j >= N)
         return 0;
     else if(abs(grid[i][j] - grid[i+1][j]) == 1)
@@ -23,10 +25,12 @@ int VerBx (int grid[N][N], int i, int j){
 void SEQUENCIA(int grid[N][N]){
 
     int i=0, j=0, dir, bx, seq=0;
-
+    
     while(j < N || i < N){
-
+        //Laço que busca e constrói uma sequência dentro da matriz
         if((VerDir(grid, i, j)) && (VerBx(grid, i, j))){
+            //Caso a posição possa ir tanto para a direita quanto para a esquerda
+            //aqui aplicamos nossa análise gulosa local para decidir a que lado ir
             dir = 0;
             bx = 0;
             dir += VerDir(grid, i, j+1);
@@ -46,18 +50,24 @@ void SEQUENCIA(int grid[N][N]){
                 j++;
         }
         else if(VerDir(grid, i, j)){
+                //Caso a posição possa ir apenas para a direita
                 printf("%d - ", grid[i][j]);
                 seq++;
                 j++;
             }
         else if (VerBx(grid, i, j)){
+                //Caso a posição possa ir apenas para a esquerda
                 printf("%d - ", grid[i][j]);
                 seq++;
                 i++;
             }
         else if(j < N && seq == 0){
+                //Caso não possa ir para nenhum lado e enquanto não iniciar nenhuma sequência
+                //vai andando as posições da matriz até encontrar uma sequencia, ou até que chegue ao final dela
                 j++;
                 if(i == N && j == N){
+                    //caso percorra toda a matriz sem achar nenhuma sequencia possivel
+                    //retorna a ultima posição como uma sequência única
                     printf("%d - ", grid[i-1][j-1]);
                     printf("\n\n\tTamanho da Sequencia: %d",seq+1);
                     break;
@@ -68,6 +78,7 @@ void SEQUENCIA(int grid[N][N]){
             j=0;
             }
         else{
+            //Quando chegar ao final de uma sequência, finaliza o algoritmo
             printf("%d - ", grid[i][j]);
             printf("\n\n\tTamanho da Sequencia: %d",seq+1);
             break;
